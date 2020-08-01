0186e900994278765dc9b7e1bdb826e14b441633
Dara1234 <tumenbayevadara@gmail.com>
2020-08-01 22:51:54 +0600

    Homework 1

--- src/Cats/RandomCatFact.js
+++ src/Cats/RandomCatFact.js
@@ -1,9 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 //https://alexwohlbruck.github.io/cat-facts/docs/
 
 function RandomCatFact() {
-  return;
+  const [catTextUrl, setCatTextUrl] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('https://cat-fact.herokuapp.com/facts')
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      const facts = data.all;
+      const randomFact = facts[Math.floor(Math.random() * facts.length)].text;
+      setCatTextUrl(randomFact);
+    });
+}, [])
+
+
+  if (catTextUrl == null) return <div> Loading </div>;
+
+  return (
+    <MainContainer>
+      <h1>{catTextUrl}</h1>
+    </MainContainer>
+  )
 }
 
 export default RandomCatFact;
