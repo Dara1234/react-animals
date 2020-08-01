f419bb854a4050d8c7ab1d18d351fb7651a9629d
Dara1234 <tumenbayevadara@gmail.com>
2020-08-01 23:39:35 +0600

    Homework 2.1

--- src/Cats/RandomCatImage.js
+++ src/Cats/RandomCatImage.js
@@ -10,25 +10,37 @@ const Image = styled.img`
   width: 100%;
 `;
 
+const Timer = styled.div``;
+
 // https://docs.thecatapi.com/
 
 function RandomCatImage(props) {
   const [catImageUrl, setCatImageUrl] = React.useState(null);
+  const [timer, setTimer] = React.useState(0);
 
   React.useEffect(() => {
-    fetch("https://api.thecatapi.com/v1/images/search")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setCatImageUrl(data[0].url);
-      });
-  }, []);
-
+    if (timer > 0) {
+      setTimeout(() => {
+        setTimer(timer -1);
+      console.log(timer);
+      }, 1000);      
+    }
+    else {
+      fetch("https://api.thecatapi.com/v1/images/search")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setCatImageUrl(data[0].url);
+        });
+        setTimer(30);
+      }
+    }, [catImageUrl, timer]);
   if (catImageUrl == null) return <div> Loading </div>;
 
   return (
     <MainContainer>
+      <Timer> New cat image will appear in {timer} seconds</Timer>
       <Image src={catImageUrl} />
     </MainContainer>
   );
